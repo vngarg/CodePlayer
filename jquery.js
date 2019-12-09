9f16585f9bb50df31e9d4ad7537833b05e730aeb
S.G <49458871+vngarg@users.noreply.github.com>
2019-12-09 19:41:00 +0530

    Working HTML

--- jquery.js
+++ jquery.js
@@ -1,6 +1,16 @@
 $(".toggleButton").hover(function(){
-    $(this).css("background-color","grey")
+    $(this).addClass("highlightedButton")
 },
 function(){
-    $(this).css("background-color","#EEEEEE")
+    $(this).removeClass("highlightedButton")
+})
+$(".toggleButton").click(function(){
+    $(this).toggleClass("active")
+    $(this).removeClass("highlightedButton")
+})
+$(".panel").height($(window).height()-$("#header").height()-14)
+$(".panel").width($(window).width()/2-4)
+
+$("textarea").on("change keyup paste",function(){
+    $("iframe").contents().find("html").html($("#htmlPanel").val())
 })
