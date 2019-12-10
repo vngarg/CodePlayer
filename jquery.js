c7bad38908023255a2f5b256f23a8afbd7bde59c
S.G <49458871+vngarg@users.noreply.github.com>
2019-12-10 23:24:03 +0530

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -4,13 +4,29 @@ $(".toggleButton").hover(function(){
 function(){
     $(this).removeClass("highlightedButton")
 })
+
+function updateOutput(){
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>"+$("#cssPanel").val()+"</style></head><body>"+$("#htmlPanel").val()+"</body></html>")
+
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val())
+}
+
 $(".toggleButton").click(function(){
     $(this).toggleClass("active")
     $(this).removeClass("highlightedButton")
+
+    var panelId=$(this).attr("id")+"Panel"
+    $("#"+panelId).toggleClass("hidden")
+
+    var numberOfActivePanels=4-$(".hidden").length
+    $(".panel").width($(window).width()/numberOfActivePanels-4)
 })
+
 $(".panel").height($(window).height()-$("#header").height()-14)
 $(".panel").width($(window).width()/2-4)
 
+updateOutput()
+
 $("textarea").on("change keyup paste",function(){
-    $("iframe").contents().find("html").html($("#htmlPanel").val())
+    updateOutput()
 })
